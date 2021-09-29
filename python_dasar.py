ca5717241b6d9f0523fe47a0dc4cc762bf47473d
RicoBasyar <RicoBasyar@users.noreply.github.com>
2021-09-29 19:57:29 +0700

    first_commit

---
+++ python_dasar.py
@@ -0,0 +1,155 @@
+# Tipe data boolean
+print(True)
+
+# Tipe data String
+print("Ini adalah string")
+
+# Tipe data Integer
+print(10+20)
+
+# Tipe data Float
+print(3.14-1.12)
+
+# Tipe data hexadesimal
+print(0x9a)
+
+# Tipe data complex
+print(5j)
+
+# Tipe data list
+print([1,2,3,4,5])
+print(['Satu','Dua','Tiga','Empat','Lima'])
+
+# Tipe data tuple
+print((1,2,3,4,5))
+print(('Satu','Dua','Tiga','Empat','Lima'))
+
+# Tipe data dictionary
+print({'Nama':'Rico', 'umur':20})
+biodata = ({'Nama':'Kawe', 'umur':21})
+print(biodata)
+
+# Check tipe data
+print(type(biodata))
+
+
+# Penulisan variabel
+namaDepan = 'Rico'
+namaBelakang = 'Basyar'
+
+print(namaDepan + ' ' + namaBelakang)
+
+# Nilai dan tipe data dalam variabel dapat diubah
+umur = 20
+print(umur)
+print(type(umur))
+umur = 'dua puluh'
+print(umur)
+print(type(umur))
+
+# Contoh penamaan variabel
+inivariabel = "Halo"
+ini_juga_variabel = "hai"
+_inivariabel = "bruh"
+inivariabel123 = "hei"
+
+panjang = 20
+lebar = 9
+luas = panjang * lebar
+print(luas)
+
+# OPERATOR ARITMATIKA
+
+# Penjumlahan
+print(1+2)
+satu = 1
+dua = 2 
+penjumlahan = satu + dua
+print(penjumlahan)
+print("\n")
+
+# Pengurangan
+print(1-2)
+satu = 1
+dua = 2 
+pengurangan = satu - dua
+print(pengurangan)
+print("\n")
+
+# Perkalian
+print(1*2)
+satu = 1
+dua = 2 
+perkalian = satu * dua
+print(perkalian)
+print("\n")
+
+# Pembagian
+print(10/2)
+sepuluh = 10
+dua = 2 
+pembagian = sepuluh / dua
+print(pembagian)
+print("\n")
+
+# Sisa bagi / Modulus
+print(15%2)
+limabelas = 15
+dua = 2 
+modulus = limabelas % dua
+print(modulus)
+print("\n")
+
+# Pangkat
+print(8 ** 3)
+delapan = 8
+tiga = 3
+pangkat = delapan ** tiga
+print(pangkat)
+print("\n")
+
+# Pembagian bulat
+print(12//5)
+duabelas = 12
+lima = 5
+pembagian_bulat = duabelas // lima
+print(pembagian_bulat)
+print("\n")
+
+# Operator perbandingan
+
+# Sama Dengan ==
+sama_dengan = 1==1
+print(1==1)
+print(sama_dengan)
+print("\n")
+
+# Tidak sama dengan != 
+tidak_sama_dengan = 1!=1
+print(tidak_sama_dengan)
+print(1!=1)
+print("\n")
+
+# Lebih besar dari >
+lebih_besar_dari = 5>2
+print(lebih_besar_dari)
+print(5>2)
+print("\n")
+
+# Lebih kecil dari <
+lebih_kecil_dari = 2<5
+print(lebih_kecil_dari)
+print(2<5)
+print("\n")
+
+# Lebih besar atau sama dengan >=
+lebih_besar_sama_dengan = 5>=5
+print(lebih_besar_sama_dengan)
+print(5>=5)
+print("\n")
+
+# Lebih kecil atau sama dengan <=
+lebih_besar_sama_dengan = 5<=5
+print(lebih_besar_sama_dengan)
+print(5<=5)
+print("\n")
