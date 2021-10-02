b1285b78425a67833e5f8c9ee8bf4881fdcdef2e
RicoBasyar <RicoBasyar@users.noreply.github.com>
2021-10-02 20:56:33 +0700

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+# Dictionary
+dict1 = {'Nama' : 'Ilham', 'Umur' : 21}
+print(dict1['Nama'])
+
+# Update nilai dictionary
+dict1['Nama'] = 'Kawe'
+print(dict1)
+
+# Hapus elemen dictionary
+del dict1['Nama']
+print(dict1)
+dict1.clear()
+print(dict1)
+del dict1
+print(dict1)
+
